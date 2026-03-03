e08e1ff572de60366aa3c084b852d3a5cc54c0bd
Darshan <darshansrujan1@gmail.com>
2026-03-04 01:25:24 +0530

    Create Event And Add Multiple TPO email and Store Separate Tables

---
+++ delegatecontact/src/main/java/com/xworkz/delegatecontact/dto/EventDTO.java
@@ -0,0 +1,30 @@
+package com.xworkz.delegatecontact.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.Transient;
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventDTO {
+    private String organizerName;
+    private String officialEmail;
+    private String organizationName;
+    private String eventTitle;
+    private String eventType;
+    private String delegateType;
+    private int targetCount;
+    private List<String> tpoEmailList;
+    private String eventMode;
+    private String eventDate;
+    private String eventLocation;
+    private String meetingLink;
+    private String eventDescription;
+    @Transient
+    private MultipartFile brochure;
+    private String brochurePath;
+}
---
+++ delegatecontact/src/main/java/com/xworkz/delegatecontact/servies/eventService/EventService.java
@@ -0,0 +1,7 @@
+package com.xworkz.delegatecontact.servies.eventService;
+
+import com.xworkz.delegatecontact.dto.EventDTO;
+
+public interface EventService {
+    boolean saveEvent(EventDTO dto);
+}
