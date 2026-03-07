b13f593d3ec6e09f3a6a944d1c0fcb3018cd14ea
Darshan <darshansrujan1@gmail.com>
2026-03-08 03:06:51 +0530

    Send Email to Event Tpo And Email contain Event Details And Tpo Login Code

---
+++ delegatecontact/src/main/java/com/xworkz/delegatecontact/servies/emailsentservice/EmailService.java
@@ -0,0 +1,54 @@
+package com.xworkz.delegatecontact.servies.emailsentservice;
+
+import com.xworkz.delegatecontact.entity.eventEntity.EventEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
+    public void sendEventToTpoEmails(String toEmail, String loginCode, EventEntity eventEntity) {
+
+        SimpleMailMessage simpleMailMessage=new SimpleMailMessage();
+        simpleMailMessage.setTo(toEmail);
+        simpleMailMessage.setSubject("Event Invitation");
+        String emailBody =
+                "Dear TPO,\n\n" +
+
+                        "Greetings from our placement team.\n\n" +
+
+                        "You are invited to participate in the following campus recruitment event:\n\n" +
+
+                        "--------------------------------------------------\n" +
+                        "Event Title : " + eventEntity.getEventTitle() + "\n" +
+                        "Event Date  : " + eventEntity.getEventDate() + "\n" +
+                        "Location    : " + eventEntity.getEventLocation() + "\n" +
+                        "Participate Target    : " + eventEntity.getTargetCount() + "\n" +
+                        "--------------------------------------------------\n\n" +
+
+                        "You can access the event portal using the following credentials:\n\n" +
+
+                        "Email      : " + toEmail + "\n" +
+                        "Login Code : " + loginCode + "\n\n" +
+
+                        "Please use this login code to access the event dashboard and confirm participation.\n\n" +
+
+                        "If you have any questions, feel free to contact our placement office.\n\n" +
+
+                        "Best Regards,\n" +
+                        "Training & Placement Cell\n" +
+                        "ABC Institute of Technology\n" +
+                        "Email: placement@abcinstitute.com\n" +
+                        "Phone: +91-9876543210\n";
+
+        simpleMailMessage.setText(emailBody);
+        javaMailSender.send(simpleMailMessage);
+
+    }
+}
