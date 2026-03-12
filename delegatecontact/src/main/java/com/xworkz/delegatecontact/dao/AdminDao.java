0c5d4389fbb23691c252ef0757129ca05d89c476
Darshan <darshansrujan1@gmail.com>
2026-03-13 02:16:26 +0530

    Get All Responses in adminDashboard And Get All Evant ,Get All Gelegate And Add Response Summary

--- delegatecontact/src/main/java/com/xworkz/delegatecontact/dao/AdminDao.java
+++ delegatecontact/src/main/java/com/xworkz/delegatecontact/dao/AdminDao.java
@@ -2,6 +2,17 @@ package com.xworkz.delegatecontact.dao;
 
 import com.xworkz.delegatecontact.entity.AdminEntity;
 
+import java.util.List;
+
 public interface AdminDao {
     AdminEntity getByEmail(String email);
+
+    int getAllEvent();
+
+    int getAllGelegate();
+
+    int getAllResponses();
+
+    List<Object[]> getEventResponses();
+
 }
--- delegatecontact/src/main/java/com/xworkz/delegatecontact/dao/AdminDaoImpl.java
+++ delegatecontact/src/main/java/com/xworkz/delegatecontact/dao/AdminDaoImpl.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
+import java.util.Collections;
+import java.util.List;
+
 @Repository
 public class AdminDaoImpl implements AdminDao{
 
@@ -33,5 +36,65 @@ public class AdminDaoImpl implements AdminDao{
         } finally {
             entityManager.close();
         }
+    }
+
+    @Override
+    public int getAllEvent() {
+        EntityManager entityManager= entityManagerFactory.createEntityManager();
+        Long countEvent;
+        try {
+            Query query = entityManager.createQuery(
+                    "SELECT COUNT(e) FROM EventEntity e");
+
+            countEvent = (Long) query.getSingleResult();
+        }finally {
+            entityManager.close();
+        }
+
+
+        return countEvent.intValue();
+    }
+
+    @Override
+    public int getAllGelegate() {
+        EntityManager entityManager= entityManagerFactory.createEntityManager();
+        Long countDelegate;
+        try {
+            Query query = entityManager.createQuery(
+                    "SELECT COUNT(d) FROM DelegateEntity d");
+
+            countDelegate = (Long) query.getSingleResult();
+        }finally {
+            entityManager.close();
+        }
+
+        return countDelegate.intValue();
+    }
+
+    @Override
+    public int getAllResponses() {
+        EntityManager entityManager= entityManagerFactory.createEntityManager();
+        Long countResponses;
+        try {
+            Query query = entityManager.createQuery(
+                    "SELECT COUNT(e) FROM RespondEntity e");
+
+             countResponses = (Long) query.getSingleResult();
+        }finally {
+            entityManager.close();
+        }
+
+
+        return countResponses.intValue();
+    }
+
+    @Override
+    public List<Object[]> getEventResponses() {
+   EntityManager entityManager= entityManagerFactory.createEntityManager();
+        Query query = entityManager.createQuery(
+                "SELECT r.eventId, COUNT(r) FROM RespondEntity r GROUP BY r.eventId"
+        );
+
+        return query.getResultList();
     }
 }
